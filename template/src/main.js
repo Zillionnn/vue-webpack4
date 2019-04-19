f39ecbaee7807c093427d87cd2e3a94294cf7322
Administrator <skyestzhang@163.com>
2019-04-19 10:56:45 +0800

    _fix

--- template/src/main.js
+++ template/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
@@ -11,6 +13,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
